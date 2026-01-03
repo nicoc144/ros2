a2727fc9acd10b709471c89e09315c56e2c2c43d
Nicolas <caballeroni7@nicolas.>
2026-01-03 23:31:55 +0100

    Gazebo world + model now working from the launchfile

--- README.md
+++ README.md
@@ -2,19 +2,39 @@
 
 
 **Instructions to install ROS2 deb packages on Ubuntu**  
-LINK: [https://docs.ros.org/en/rolling/Installation/Ubuntu-Install-Debs.html](https://docs.ros.org/en/rolling/Installation/Ubuntu-Install-Debs.html)
+LINK: [https://docs.ros.org/en/kilted/Installation/Ubuntu-Install-Debs.html](https://docs.ros.org/en/kilted/Installation/Ubuntu-Install-Debs.html)
 
 **Install the build tool for ROS2**
 ```
 sudo apt install python3-colcon-common-extensions
 ```
 
+**Install Gazebo for ROS2 Kilted**
+```
+sudo apt-get install ros-kilted-ros-gz
+```
+
 **Run this command on every new shell to have access to ROS 2 commands**
 ``` 
-source /opt/ros/rolling/setup.bash
+source /opt/ros/kilted/setup.bash
 ```
  
 **Or add this to the shell startup script** 
 ```
-echo "source /opt/ros/rolling/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/kilted/setup.bash" >> ~/.bashrc
+```
+
+**Run this command inside the 'ros_ws' directory to compile the workspace packages**
+```
+colcon build
+```
+
+**Run this command inside the 'ros_ws' directory to make ROS aware of the newly compiled packages**
+```
+source /install/setup.bash
+```
+
+**Run this command inside the 'ros_ws' directory to view the simulation**
+```
+ros2 launch diff_drive bot_launch.py
 ```
--- ros2_ws/src/diff_drive/launch/bot_launch.py
+++ ros2_ws/src/diff_drive/launch/bot_launch.py
@@ -1,13 +1,43 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    # Get paths (good for portability of code)
+    pkg_path = get_package_share_directory('diff_drive')
+    world_path = os.path.join(pkg_path, 'worlds/test_world.sdf')
+    bot_model_path = os.path.join(pkg_path, 'models/diff_drive_bot/diff_drive.sdf')
+
     return LaunchDescription([
+
+        # Launch gazebo with the custom world    
+        ExecuteProcess(
+            cmd = ['gz', 'sim', world_path], # cmd = [executable, arg1, arg2, ...]
+            output = 'screen'
+        ),
+
+        # Launch the differential drive bot in the custom world
+        Node(
+            package = 'ros_gz_sim',
+            executable = 'create', # Used to place the model into the world
+            arguments = ['-file', bot_model_path, '-name', 'diff_drive_bot'],
+                # Valued arguments (ex: -name 'my_node'):
+                # arguments = [flag1, value1, flag2, value2]
+                # Bool arguments (ex: -allow_renaming):
+                # arguments = [flag]
+            output = 'screen'
+        ),
+
+        # Publisher node
         Node(
             package = 'diff_drive',
             executable = 'velop',
             name = 'velocity_publisher'
         ),
+
+        # Subscriber node
         Node(
             package = 'diff_drive',
             executable = 'velos',
